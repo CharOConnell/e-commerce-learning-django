84e6d2387040a5377c20c74edb15d3e677e683ed
CharOConnell <charlotteoconnell@virginmedia.com>
2020-06-04 08:25:26 +0000

    customised admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,29 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    # tuple which shows which fields to display
+    # now let's sort by stu, but despite it only being
+    # one variable, it still has to be a tuple
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+# we need to then register the classes next to their respective models
