61cfd479fe9f27dfcfa5c4b368dcaded4c2f4e32
CharOConnell <charlotteoconnell@virginmedia.com>
2020-06-04 11:39:46 +0000

    added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -24,12 +24,13 @@ def all_products(request):
                 if sortkey == 'name':
                     sortkey = 'lower_name'
                     products = products.annotate(lower_name=Lower('name'))
-
+                if sortkey == 'category':
+                    sortkey = 'caetgory__name'
+                    # allows us to drill into the related model
                 if 'direction' in request.GET:
                     direction = request.GET['direction']
                     if direction == 'desc':
                         sortkey = f'-{sortkey}'
-
                 products = products.order_by(sortkey)
 
         # if it exists, we need to show only the filtered products
