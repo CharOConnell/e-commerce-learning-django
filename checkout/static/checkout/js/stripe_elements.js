720b5c0703c0a26d19f187e617ce1b09c5ee37d2
CharOConnell <charlotteoconnell@virginmedia.com>
2020-06-15 09:29:09 +0000

    added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,35 @@
+/* 
+    Core login/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here:
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+// remove the apostrophes from either end
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#000',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#dc3545',
+    iconColor: '#dc3545'
+  }
+};
+
+// instance of stripe elements
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
