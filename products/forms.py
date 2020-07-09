642667e5d35f88f1262adda2b62b1ebac9f96d54
CharOConnell <charlotteoconnell@virginmedia.com>
2020-07-09 08:51:59 +0000

    Product Admin - Product Form

---
+++ products/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+        # includes all the fields
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        # this is a nice way to create a list of tuples
+        # syntax is called list comprehension so the for loop adds to the list
+
+        self.fields['category'].choices = friendly_names
+        # form will use the friendly names on the form
+        for field_name, field in self.felds.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+            # style the same as the rest of the site
